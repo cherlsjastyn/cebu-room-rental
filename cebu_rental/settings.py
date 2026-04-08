4716099c84d54985961825390381b80b56d0e3c9
Charles Taneo <charlestaneo198@gmail.com>
2026-04-09 01:50:03 +0800

    Final URL

--- cebu_rental/settings.py
+++ cebu_rental/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n367@&-r8ezf&j=yud=6&1415h15ol+8dbf)uqpvhg28t_jck8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://cebu-room-rental.onrender.com']
+ALLOWED_HOSTS = ['https://cebu-room-rental-final.onrender.com']
 
 
 # Application definition
